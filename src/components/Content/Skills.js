faaae7e94d2b7b0543867414b37c080a91d04479
ssazero <samukedo@gmail.com>
2020-04-26 20:18:12 +0200

    technology groups

--- src/components/Content/Skills.js
+++ src/components/Content/Skills.js
@@ -6,12 +6,10 @@ import Technology from "../Technology";
 import css3 from "../../assets/images/technologies/css3.png";
 import git from "../../assets/images/technologies/git.png";
 import html5 from "../../assets/images/technologies/html5.png";
-import java from "../../assets/images/technologies/java.png";
 import js from "../../assets/images/technologies/js.png";
 import mongodb from "../../assets/images/technologies/mongodb.png";
 import mysql from "../../assets/images/technologies/mysql.png";
 import nodejs from "../../assets/images/technologies/nodejs.png";
-import php from "../../assets/images/technologies/php.png";
 import ps from "../../assets/images/technologies/ps.png";
 import reactNative from "../../assets/images/technologies/react-native.png";
 import react from "../../assets/images/technologies/react.png";
@@ -34,23 +32,38 @@ const Skills = (props) => {
         </span>
       </h2>
       <div className="technologies">
-        <Technology src={react} name="React" />
-        <Technology src={js} name="JavaScript ES6+" />
-        <Technology src={nodejs} name="Node.js" />
-        <Technology src={html5} name="HTML5" />
-        <Technology src={css3} name="CSS3" />
-        <Technology src={sass} name="Sass" />
-        <Technology src={redux} name="Redux" />
-        <Technology src={reactNative} name="React Native" />
-        <Technology src={git} name="Git" />
-        <Technology src={mongodb} name="MongoDB" />
-        <Technology src={mysql} name="MySQL" />
-        <Technology src={vsc} name="Visual Studio Code" />
-        <Technology src={ps} name="Photoshop" />
-        <Technology src={symfony} name="Symfony 4" />
-        <Technology src={php} name="php" />
-        <Technology src={springBoot} name="Spring Boot" />
-        <Technology src={java} name="Java" />
+        <div className="technologies-group">
+          <h3 className="technologies-group__title">FRONT-END</h3>
+          <div className="technologies-group__items">
+            <Technology src={react} name="React" />
+            <Technology src={js} name="JavaScript ES6+" />
+            <Technology src={redux} name="Redux" />
+            <Technology src={html5} name="HTML5" />
+            <Technology src={css3} name="CSS3" />
+            <Technology src={sass} name="Sass" />
+          </div>
+        </div>
+        <div className="technologies-group">
+          <h3 className="technologies-group__title">BACK-END</h3>
+          <div className="technologies-group__items">
+            <Technology src={nodejs} name="Node.js / Express.js" />
+            <Technology src={mongodb} name="MongoDB" />
+            <Technology src={mysql} name="MySQL" />
+            <Technology src={springBoot} name="Spring Boot / Java" />
+            <Technology src={symfony} name="Symfony 4 / php" />
+          </div>
+        </div>
+        <div className="technologies-group">
+          <h3 className="technologies-group__title">
+            {context.dictionary.skills.other}
+          </h3>
+          <div className="technologies-group__items">
+            <Technology src={reactNative} name="React Native" />
+            <Technology src={git} name="Git" />
+            <Technology src={vsc} name="Visual Studio Code" />
+            <Technology src={ps} name="Photoshop" />
+          </div>
+        </div>
       </div>
     </section>
   );

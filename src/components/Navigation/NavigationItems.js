a5010946fe7940fac7207dd19959a87270fab23a
ssazero <samukedo@gmail.com>
2020-04-28 02:21:59 +0200

    fixed bugs

--- src/components/Navigation/NavigationItems.js
+++ src/components/Navigation/NavigationItems.js
@@ -7,19 +7,19 @@ const NavigationItems = props => {
 	const context = useContext(LanguageContext);
 	return (
 		<ul className="navigation">
-			<NavigationItem to="top" offset={-50} opened={props.opened} clicked={props.clicked}>
+			<NavigationItem to="top" offset={-10} opened={props.opened} clicked={props.clicked}>
 				{context.dictionary.nav.home}
 			</NavigationItem>
-			<NavigationItem to="section-about" offset={-50} opened={props.opened} clicked={props.clicked}>
+			<NavigationItem to="section-about" offset={-10} opened={props.opened} clicked={props.clicked}>
 				{context.dictionary.nav.about}
 			</NavigationItem>
-			<NavigationItem to="section-skills" offset={-50} opened={props.opened} clicked={props.clicked}>
+			<NavigationItem to="section-skills" offset={-10} opened={props.opened} clicked={props.clicked}>
 				{context.dictionary.nav.skills}
 			</NavigationItem>
-			<NavigationItem to="section-offer" offset={-50} opened={props.opened} clicked={props.clicked}>
+			<NavigationItem to="section-offer" offset={-10} opened={props.opened} clicked={props.clicked}>
 				{context.dictionary.nav.offer}
 			</NavigationItem>
-			<NavigationItem to="section-portfolio" offset={-30} opened={props.opened} clicked={props.clicked}>
+			<NavigationItem to="section-portfolio" offset={-10} opened={props.opened} clicked={props.clicked}>
 				{context.dictionary.nav.portfolio}
 			</NavigationItem>
 		</ul>

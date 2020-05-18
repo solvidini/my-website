12d8d5aba6f3b182c40446c97d976ea5d0bdea36
ssazero <samukedo@gmail.com>
2020-05-18 23:37:31 +0200

    loading screen

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './sass/main.scss';
+import Spinner from './components/UI/Spinner/Spinner';
 import Layout from './containers/Layout';
 import Content from './components/Content/Content';
+import logo from './assets/images/logo.png';
 
-console.log("Thanks for particles effect to: https://github.com/Wufe/react-particles-js")
+console.log(
+  'Thanks for particles effect to: https://github.com/Wufe/react-particles-js'
+);
 
 function App() {
-	return (
-		<Layout>
-			<Content />
-		</Layout>
-	);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 900);
+  }, []);
+
+  let appContent;
+
+  if (loading) {
+    appContent = (
+      <div className="loadingScreen">
+        <img className="loadingScreen__logo" src={logo} alt="SS" />
+        <Spinner />
+      </div>
+    );
+  } else {
+    appContent = (
+      <Layout>
+        <Content />
+      </Layout>
+    );
+  }
+
+  return appContent;
 }
 
 export default App;
